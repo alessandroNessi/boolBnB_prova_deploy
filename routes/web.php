75a27a5eea065a49994e633444cff56a7a2f9ee7
alessandroNessi <alessandronessi@msn.com>
2022-01-03 19:59:24 +0100

    fix casuale

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
+Route::post('/messages/store/{id}', 'MessageController@store')->name('message.store');
+Route::get('/guest', 'PageController@index')->name('guest.index');
 
 Auth::routes();
 
@@ -43,6 +45,4 @@ Route::middleware('auth')->namespace('Admin')->name('admin.')->prefix('admin')->
 });
 
 // rotte pubbliche
-Route::get('/{any}', 'PageController@index')->where('any', '.*')->name('guest.index');
-Route::post('/messages/store/{id}', 'MessageController@store')->name('message.store');
-// Route::get('/guest', 'PageController@index')->name('guest.index');
+// Route::get('/{any}', 'PageController@index')->where('any', '.*')->name('guest.index');
